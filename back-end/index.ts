8301adfe680d674294a4643dab4a1dc1a64828a5
Rodrigo Sales <rodrigosalesveloso@gmail.com>
2026-07-29 22:21:06 -0300

    conectando back-end com front-end e criando uma api de request

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { connection } from "./src/db.js";
 import { prisma } from "./src/db.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 connection();
 
 app.post("/login", async (req, res) => {
