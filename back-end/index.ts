9dcec97526f04b3b868268ce5decd80d78247a2e
Rodrigo Sales <rodrigosalesveloso@gmail.com>
2026-09-14 17:45:37 -0300

    começando a tratar os registers de users

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 import { connection } from "./src/db.js";
 import { prisma } from "./src/db.js";
 import cors from "cors";
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 connection();
 
-app.post("/login", async (req, res) => {
+app.post("/login", async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
@@ -30,6 +30,28 @@ app.post("/login", async (req, res) => {
     res.status(500).json({ message: "Erro no servidor" });
   }
 });
+
+app.post("/register", async (req: Request, res:Response) => {
+
+  try {
+    const {name, email, password, cep} = req.body;
+
+    if (!name || !email || !password || !cep) {
+      res.status(400).json({message:"Todas as informações são obrigatórias"});
+      return;
+    }
+
+    const newUser = await prisma.user.create({
+      data: {name: name, email: email, password: password, cep: cep},
+    });
+
+    res.json(newUser);
+
+  } catch (error) {
+    res.status(500).json({message:"Erro no servidor"})
+    return;
+  }
+})
 
 app.listen(3000, () => {
   console.log("server rodando na porta 3000-1");
